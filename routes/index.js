1842e07555da6cccdd5c05f4946456e7eb8690b1
Chris Fried <fried.chris@gmail.com>
2018-11-06 16:36:53 -0700

    no console log

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,8 @@ setInterval(() => (cache = {}), 30000);
 // Courtesy of https://github.com/DestinyTrialsReport/DestinyTrialsReport/blob/05c113f8d39dee2a02461902f0c9e1c287cad3aa/server.js#L37
 router.get('/Results/*?', function(req, res) {
   if (cache[req.originalUrl]) {
-    console.log('cache!');
     res.send(cache[req.originalUrl]);
   } else {
-    console.log('fresh');
     res.setTimeout(25000);
     var options = {
       url: 'http://electionresults.sos.state.mn.us/' + req.originalUrl
