65d3bf844d98d52d47cb21e015a24e8396c1bc75
Chris Fried <fried.chris@gmail.com>
2018-11-06 16:34:59 -0700

    Cache

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,36 @@ var router = express.Router();
 var request = require('request');
 var fs = require('fs');
 
-router.get('/', function (req, res) {
+router.get('/', function(req, res) {
   res.render('index');
 });
 
+var cache = {};
+
+setInterval(() => (cache = {}), 30000);
+
 // Courtesy of https://github.com/DestinyTrialsReport/DestinyTrialsReport/blob/05c113f8d39dee2a02461902f0c9e1c287cad3aa/server.js#L37
-router.get('/Results/*?', function (req, res) {
-  res.setTimeout(25000);
-  var options = {
-    url: 'http://electionresults.sos.state.mn.us/' + req.originalUrl
-  };
-  try {
-    request(options, function (error, response, body) {
-      if (!error) {
-        res.send(body);
-      } else {
-        res.send(error);
-      }
-    });
-  } catch (e) {}
+router.get('/Results/*?', function(req, res) {
+  if (cache[req.originalUrl]) {
+    console.log('cache!');
+    res.send(cache[req.originalUrl]);
+  } else {
+    console.log('fresh');
+    res.setTimeout(25000);
+    var options = {
+      url: 'http://electionresults.sos.state.mn.us/' + req.originalUrl
+    };
+    try {
+      request(options, function(error, response, body) {
+        if (!error) {
+          cache[req.originalUrl] = body;
+          res.send(body);
+        } else {
+          res.send(error);
+        }
+      });
+    } catch (e) {}
+  }
 });
 
 module.exports = router;
